4f45d06b7dcfed55c9f80012af37d47039006606
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2023-07-28 17:17:23 -0500

    implement authentication with external api integration

--- pages/api/messages/admin.ts
+++ pages/api/messages/admin.ts
@@ -1,16 +1,43 @@
-import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { AxiosRequestConfig } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import { AppError } from "../../../models/app-error";
 import { Message } from "../../../models/message";
+import { callExternalApi } from "../../../services/external-api.service";
+
+const apiServerUrl = process.env.API_SERVER_URL;
 
 const getAdminMessage = async (
   req: NextApiRequest,
-  res: NextApiResponse<Message>
+  res: NextApiResponse<Message | AppError>
 ) => {
-  const message: Message = {
-    text: "This is an admin message.",
-  };
+  try {
+    const { accessToken } = await getAccessToken(req, res);
 
-  res.status(200).json(message);
+    const config: AxiosRequestConfig = {
+      url: `${apiServerUrl}/api/messages/admin`,
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    const { data, error, status } = await callExternalApi<Message>({
+      config,
+    });
+
+    if (data) {
+      res.status(status).json(data);
+      return;
+    }
+
+    res
+      .status(status || 500)
+      .json(error || { message: "Unable to retrieve message" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export default withApiAuthRequired(getAdminMessage);
